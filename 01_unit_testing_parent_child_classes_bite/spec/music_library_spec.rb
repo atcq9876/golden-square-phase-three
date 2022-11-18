188373ec00dc259bafed1779d2d43a51802f0bc3
Andy <ajscottlewis@gmail.com>
2022-11-18 11:53:54 +0000

    Write code and tests for both classes (except mock tests)

--- 01_unit_testing_parent_child_classes_bite/spec/music_library_spec.rb
+++ 01_unit_testing_parent_child_classes_bite/spec/music_library_spec.rb
@@ -1,2 +1,103 @@
 require 'music_library'
 
+
+# Mock tests
+
+
+
+
+
+# Other unit tests
+
+describe "#all" do
+  context "when no tracks added" do
+    it "fails" do
+      library = MusicLibrary.new
+      expect{library.all}.to raise_error "No tracks added yet"
+    end
+  end
+end
+
+describe "#search" do
+  context "when no tracks added" do
+    it "fails" do
+      library = MusicLibrary.new
+      expect{library.search("test")}.to raise_error "No tracks added yet"
+    end
+  end
+end
+
+
+
+
+
+
+
+# general tests
+
+  # 1
+    #all - no tracks added
+
+  # 2
+    #search - no tracks added
+
+
+
+
+# mock tests (base on integration tests)
+
+# describe "integration" do
+#   describe "MusicLibrary's #add and #all, and related DiaryEntry methods" do
+#     context "when adding two tracks and calling #all" do
+#       it "returns both tracks" do
+#         track_1 = Track.new("Steve Biko", "A Tribe Called Quest")
+#         track_2 = Track.new("Award Tour", "Tribe")
+#         library = MusicLibrary.new
+#         library.add(track_1)
+#         library.add(track_2)
+#         expect(library.all).to eq [track_1, track_2]
+#       end
+#     end
+#   end
+
+#   describe "MusicLibrary's #search and #add, and related DiaryEntry methods" do
+#     context "add three tracks and search for a keyword contained in one track" do
+#       it "returns said track" do
+#         track_1 = Track.new("Steve Biko", "A Tribe Called Quest")
+#         track_2 = Track.new("Award Tour", "Tribe")
+#         track_3 = Track.new("Ego Trippin'", "De La Soul")
+#         library = MusicLibrary.new
+#         library.add(track_1)
+#         library.add(track_2)
+#         library.add(track_3)
+#         expect(library.search("Tour")).to eq [track_2]
+#       end
+#     end
+
+#     context "add three tracks and search for a keyword that doesn't match" do
+#       it "raises error" do
+#         track_1 = Track.new("Steve Biko", "A Tribe Called Quest")
+#         track_2 = Track.new("Award Tour", "Tribe")
+#         track_3 = Track.new("Ego Trippin'", "De La Soul")
+#         library = MusicLibrary.new
+#         library.add(track_1)
+#         library.add(track_2)
+#         library.add(track_3)
+#         expect{library.search("test")}.to raise_error "No tracks contain that keyword"
+#       end
+#     end
+
+#     context "add three tracks and search for non-string keyword" do
+#       it "raises error" do
+#         track_1 = Track.new("Steve Biko", "A Tribe Called Quest")
+#         track_2 = Track.new("Award Tour", "Tribe")
+#         track_3 = Track.new("Ego Trippin'", "De La Soul")
+#         library = MusicLibrary.new
+#         library.add(track_1)
+#         library.add(track_2)
+#         library.add(track_3)
+#         expect{library.search(5)}.to raise_error "Please enter a valid keyword"
+#       end
+#     end
+#   end
+# end
--- 01_unit_testing_parent_child_classes_bite/spec/track_spec.rb
+++ 01_unit_testing_parent_child_classes_bite/spec/track_spec.rb
@@ -1,2 +1,55 @@
 require 'track'
 
+
+describe "#initialize" do
+  context "when passed valid arguments" do
+    it "successfully constructs" do
+      track = Track.new("Award Tour", "Tribe")
+      expect(track.is_a? Track).to eq true
+    end
+  end
+
+  context "when passed non-string arguments" do
+    it "raises error" do
+      expect{track = Track.new(5, nil)}.to raise_error "Please enter a valid track"
+    end
+  end
+
+  context "when passed empty argument(s)" do
+    it "raises error" do
+      expect{track = Track.new("", "Test")}.to raise_error "Please enter a valid track"
+    end
+  end      
+end
+
+describe "#matches?" do
+  context "with track that contains keyword" do
+    it "returns true" do
+      track = Track.new("Award Tour", "Tribe")
+      expect(track.matches?("Tour")).to eq true
+    end
+  end
+end
+
+describe "#matches?" do
+  context "with track that doesn't contain keyword" do
+    it "returns false" do
+      track = Track.new("Award Tour", "Tribe")
+      expect(track.matches?("Test")).to eq false
+    end
+  end
+
+  context "with non-string keyword passed to method" do
+    it "fails" do
+      track = Track.new("Award Tour", "Tribe")
+      expect{track.matches?(2)}.to raise_error "Please enter a valid keyword"
+    end
+  end
+
+  context "with empty string keyword passed to method" do
+    it "fails" do
+      track = Track.new("Award Tour", "Tribe")
+      expect{track.matches?("")}.to raise_error "Please enter a valid keyword"
+    end
+  end
+end
