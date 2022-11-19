83b77f82be1c2be62199bfb92ca170d0725eab02
Andy <ajscottlewis@gmail.com>
2022-11-19 17:17:30 +0000

    Add error tests

--- 05_unit_testing_terminal_io_bite/exercise/lib/interactive_calculator.rb
+++ 05_unit_testing_terminal_io_bite/exercise/lib/interactive_calculator.rb
@@ -8,16 +8,27 @@ class InteractiveCalculator
   def run
     @terminal.puts "Hello. I will subtract two numbers."
     @terminal.puts "Please enter a number"
-    first_number = @terminal.gets.to_i
+    first_number = get_number
     @terminal.puts "Please enter another number"
-    second_number = @terminal.gets.to_i
+    second_number = get_number
     @terminal.puts "Here is your result:"
     @terminal.puts "#{first_number} - #{second_number} = #{first_number - second_number}"
   end
+
+  private
+
+  def get_number
+    response = @terminal.gets
+    if response.to_i.to_s == response
+      return response.to_i
+    else
+      fail "You must enter a number"
+    end
+  end
 end
 
-interactive_calculator = InteractiveCalculator.new(Kernel)
-interactive_calculator.run
+# interactive_calculator = InteractiveCalculator.new(Kernel)
+# interactive_calculator.run
 
 # Hello. I will subtract two numbers.
 # Please enter a number
--- 05_unit_testing_terminal_io_bite/exercise/spec/interactive_calculator_spec.rb
+++ 05_unit_testing_terminal_io_bite/exercise/spec/interactive_calculator_spec.rb
@@ -5,12 +5,34 @@ RSpec.describe InteractiveCalculator do
     terminal = double :terminal
     expect(terminal).to receive(:puts).with("Hello. I will subtract two numbers.").ordered
     expect(terminal).to receive(:puts).with("Please enter a number").ordered
-    expect(terminal).to receive(:gets).and_return(4).ordered
+    expect(terminal).to receive(:gets).and_return("4").ordered
     expect(terminal).to receive(:puts).with("Please enter another number").ordered
-    expect(terminal).to receive(:gets).and_return(3).ordered
+    expect(terminal).to receive(:gets).and_return("3").ordered
     expect(terminal).to receive(:puts).with("Here is your result:").ordered
     expect(terminal).to receive(:puts).with("4 - 3 = 1").ordered
     calculator = InteractiveCalculator.new(terminal)
     calculator.run
+  end
+
+  it "raises error if string entered instead of number for first gets" do
+    terminal = double :terminal
+    expect(terminal).to receive(:puts).with("Hello. I will subtract two numbers.").ordered
+    expect(terminal).to receive(:puts).with("Please enter a number").ordered
+    expect(terminal).to receive(:gets).and_return("test").ordered
+    
+    calculator = InteractiveCalculator.new(terminal)
+    expect{ calculator.run }.to raise_error "You must enter a number"
+  end
+
+  it "raises error if string entered instead of number for second gets" do
+    terminal = double :terminal
+    expect(terminal).to receive(:puts).with("Hello. I will subtract two numbers.").ordered
+    expect(terminal).to receive(:puts).with("Please enter a number").ordered
+    expect(terminal).to receive(:gets).and_return("4").ordered
+    expect(terminal).to receive(:puts).with("Please enter another number").ordered
+    expect(terminal).to receive(:gets).and_return("Test").ordered
+    
+    calculator = InteractiveCalculator.new(terminal)
+    expect{ calculator.run }.to raise_error "You must enter a number"
   end
 end
