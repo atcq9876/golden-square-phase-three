dd32ad33b83d0b18bdf084965be528d5f1875f77
Andy <ajscottlewis@gmail.com>
2022-11-19 15:03:51 +0000

    Complete exercise

--- 04_unit_testing_api_requests_bite/exercise/lib/api_exercise.rb
+++ 04_unit_testing_api_requests_bite/exercise/lib/api_exercise.rb
@@ -2,23 +2,36 @@
 
 require 'date'
 require 'json'
-require 'net/http'
 
 class TimeError
+  def initialize(requester)
+    @requester = requester
+  end  
+  
   # Returns difference in seconds between server time
   # and the time on this computer
-  def error
-    return get_server_time - Time.now
+  def error(current_time)
+    get_server_time - current_time
   end
 
   private
 
   def get_server_time
-    text_response = Net::HTTP.get(URI("https://worldtimeapi.org/api/ip"))
+    text_response = @requester.get(URI("https://worldtimeapi.org/api/ip"))
     json = JSON.parse(text_response)
+    DateTime.parse(json["utc_datetime"]).to_time
     return DateTime.parse(json["utc_datetime"]).to_time
   end
 end
+
+
+# Usage
+# # -----------
+require 'net/http'
+time_error = TimeError.new(Net::HTTP)
+time_error.error(Time.now)
+
+
 
 # To make this testable, you will need to create a double for Net::HTTP and
 # also deal with the behaviour of Time.now which is also non-deterministic.
--- 04_unit_testing_api_requests_bite/exercise/spec/api_exercise_spec.rb
+++ 04_unit_testing_api_requests_bite/exercise/spec/api_exercise_spec.rb
@@ -1,2 +1,15 @@
 require "api_exercise"
+require "date"
+# require "json"
 
+RSpec.describe TimeError do
+  it "calculates delay of server time" do
+    fake_requester = double :requester
+    expect(fake_requester).to receive(:get).with(
+      URI("https://worldtimeapi.org/api/ip")
+      ).and_return('{"abbreviation":"GMT","client_ip":"2a00:23c4:8c92:cc01:a57b:dd76:3253:cd","datetime":"2022-11-19T14:13:05.699431+00:00","day_of_week":6,"day_of_year":323,"dst":false,"dst_from":null,"dst_offset":0,"dst_until":null,"raw_offset":0,"timezone":"Europe/London","unixtime":1668867185,"utc_datetime":"2022-11-19T14:13:05.699431+00:00","utc_offset":"+00:00","week_number":46}')
+    time_error = TimeError.new(fake_requester)
+    time = Time.new(2022, 11, 19, 14, 13, 7)
+    expect(time_error.error(time)).to eq -1.300569
+  end
+end
